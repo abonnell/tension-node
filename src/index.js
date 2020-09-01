bda7132a4b546e07d4f4e8d71b7efc12c7243765
Andrew Bonnell <andrew.bonnell@phoenix.edu>
2020-08-31 18:07:28 -0700

    add todo

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ class App extends React.Component {
           />
         </div>
         <div className='button-holder'>
+          {/* TODO onclick handler */}
           <TensionButton pool={this.state.currentPool} />
           <TensionButton />
           <TensionButton />
